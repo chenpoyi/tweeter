bd2aec78888a2f915f9d42f65fe670c08524bf36
Paul Chen <paulchen647@gmail.com>
2020-10-06 21:20:01 +0000

    Implement char counter and add color change

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  //console.log("CHAR COUNTER READY");
+
+  $( '#tweet-text').keyup(
+    function(event){
+      //console.log($( this ).val().length);
+      //console.log($( this ).siblings("div").children(".counter").val());
+      //let counterValue = $( this ).siblings("div").children(".counter").val();
+      const currentLength = 140 - $( this ).val().length
+      const counter = $( this ).siblings("div").children(".counter");
+      counter.val(currentLength);
+
+      //console.log(counterValue);
+      if(currentLength < 0){
+        //console.log("NEGATIVE");
+        //changes color to red if negative
+        counter.addClass("alert");
+      } else {
+        counter.removeClass("alert");
+      }
+    }
+  )
+});
