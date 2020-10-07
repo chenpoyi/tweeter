f3fbbfa56bbfb982b868bb4e64f28b9ef0044d6c
Paul Chen <paulchen647@gmail.com>
2020-10-07 21:22:21 +0000

    Fetch tweets and display on page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,23 @@
  */
 
 $(document).ready(function() {
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    $('#tweets-container').empty();
+
+    for( let tweet of tweets){
+
+      const $tweet = createTweetElement(tweet);
+
+
+    $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    }
+  }
+
+
   const createTweetElement = function(tweet){
     const user = tweet.user;
     const content = tweet.content;
@@ -36,15 +53,26 @@ $(document).ready(function() {
   }
 
   const timeAgo = function(dateTime){
-    console.log('NOW: ', new Date().toString());
-    console.log('Time: ', new Date(dateTime).toString());
+    
 
     const now = new Date();
     const time = new Date(dateTime);
     const diff = now - time;
     const diffDays = Math.ceil(diff / (1000 * 60 * 60 * 24));
-    console.log(diffDays);
     return diffDays;
+  }
+
+  const loadTweets = function(){
+    $.ajax('/tweets', { method: 'GET'})
+    .done(
+      function(data){
+        console.log(data);
+        renderTweets(data);
+        
+      }
+    )
+  
+
   }
 
 
@@ -52,28 +80,53 @@ $(document).ready(function() {
 
 
 
-
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-const $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
-timeAgo(tweetData.created_at);
+  $("#tweet-form").submit(function(event){
+    event.preventDefault();
+    const text = $(this).serialize();
+    console.log('tweet: ', text);
+    $.ajax('/tweets', { method: 'POST', data: text}).
+    done(function(){
+      loadTweets();
+    })
+    .catch(function(err){
+      console.log("ERROR: ", err);
+    })
+    
+  });
 
+
+//const tweets = 
+//console.log(loadTweets());
+//renderTweets(tweets);
+loadTweets();
 
 });
 
