91f79c3b0eef37bb498b8d53ee42d2b47f6d34eb
Paul Chen <paulchen647@gmail.com>
2020-10-07 00:17:54 +0000

    Implement tweet container with css

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
   // --- our code goes here ---
   //console.log("CHAR COUNTER READY");
 
-  $( '#tweet-text').keyup(
-    function(event){
+  $('#tweet-text').keyup(
+    function(event) {
       //console.log($( this ).val().length);
       //console.log($( this ).siblings("div").children(".counter").val());
       //let counterValue = $( this ).siblings("div").children(".counter").val();
-      const currentLength = 140 - $( this ).val().length
-      const counter = $( this ).siblings("div").children(".counter");
+      const currentLength = 140 - $(this).val().length;
+      const counter = $(this).siblings("div").children(".counter");
       counter.val(currentLength);
 
       //console.log(counterValue);
-      if(currentLength < 0){
+      if (currentLength < 0) {
         //console.log("NEGATIVE");
         //changes color to red if negative
         counter.addClass("alert");
@@ -20,5 +20,5 @@ $(document).ready(function() {
         counter.removeClass("alert");
       }
     }
-  )
+  );
 });
